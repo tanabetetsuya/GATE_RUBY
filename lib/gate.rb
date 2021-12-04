c6a73a0151bbe99683e50d77a3efeec6898a78fc
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-04 23:33:11 +0900

    first commit

---
+++ lib/gate.rb
@@ -0,0 +1,13 @@
+class Gate
+end
+
+#umeda = Gate.new(:umeda)
+#mikuni = Gate.new(:mikuni)
+
+#ticket = Ticket.new(150)
+#umeda.enter(ticket)
+#mikuni.exit(ticket)
+
+#ticket = Ticket.new(190)
+#umeda.enter(ticket)
+#mikuni.exit(ticket)
