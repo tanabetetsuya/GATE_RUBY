14b5e04d26168a3220e6bd436bc0dcf2d359f19e
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-05 09:58:28 +0900

    [Add] enter,exitメソッドの定義

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,5 +1,19 @@
 class Gate
+  def initialize(station)
+    @station = station
+  end
+
+  def enter(ticket)
+  end
+
+  def exit(ticket)
+    true
+  end
+
+
 end
+
+  
 
 #umeda = Gate.new(:umeda)
 #mikuni = Gate.new(:mikuni)
