e3ae8b780519e14fb3d751717898608602c3ffb8
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-05 12:57:55 +0900

    [Add] enter,exit,culc_fareの定義

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,13 +1,25 @@
 class Gate
+  STATIONS = [:umeda, :juso, :mikuni]
+  FARES = [150, 190]
+
   def initialize(station)
     @station = station
   end
 
   def enter(ticket)
+    #Ticketクラスのstampメソッドを呼び出し、自分の駅名を渡す
+    ticket.stamp(@station)
   end
 
   def exit(ticket)
-    true
+    fare = calc_fare(ticket)
+  end
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@station)
+    destance = to - from
+    FARES[distance - 1]
   end
 
 
