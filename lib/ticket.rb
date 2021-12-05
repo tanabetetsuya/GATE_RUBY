80c3a2d3b3a05a71cde664f4cc884f9a1affae78
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-05 13:18:40 +0900

    [Update] @stamped => @stamped_at

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,11 +1,11 @@
 class Ticket
   attr_reader :fare, :stamped_at
-  
+
   def initialize(fare)
     @fare = fare
   end
 
   def stamp(station)
-    @stamped = station
+    @stamped_at = station
   end
 end
